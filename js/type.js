dfb163e5266069734aedeb833748f7b3491c9288
Tebib Fethallah <fethotebib3@gmail.com>
2023-02-22 15:41:35 +0100

    added the typeing animation and change lang

---
+++ js/type.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.typing',{
+    strings :["management","communiation","marketing & commercial","ressources humains","systeme d'information","achats"]
+    , typeSpeed : 100,
+    backSpeed: 60,
+    loop:true
+})
