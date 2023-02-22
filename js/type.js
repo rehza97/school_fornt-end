7b571c101e7abb75a3492878617c3eb92eb48212
Tebib Fethallah <fethotebib3@gmail.com>
2023-02-22 16:32:44 +0100

    fix the bug

--- js/type.js
+++ js/type.js
@@ -1,6 +1,1 @@
-var typed = new Typed('.typing',{
-    strings :["management","communiation","marketing & commercial","ressources humains","systeme d'information","achats"]
-    , typeSpeed : 100,
-    backSpeed: 60,
-    loop:true
-})
+
